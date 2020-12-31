b387ba89e39a5fb76e3172ca84a5fe5bbbe10962
cheolhwan <publizm@gmail.com>
2020-12-31 16:19:41 +0900

    scrollview 컴포넌트의 props 활용

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 import Header from './src/header';
 import Generator from './src/generator';
 import NumList from './src/numlist';
@@ -30,8 +30,6 @@ class App extends Component {
       (random, index) => index !== idx,
     );
 
-    alert(changedRandom);
-
     this.setState((prevState) => ({
       random: changedRandom,
     }));
@@ -49,8 +47,23 @@ class App extends Component {
             hello world
           </Text>
         </View>
-        <Generator onAddRandomNum={this.onAddRandomNum} />
-        <NumList onNumDelete={this.onNumDelete} numbers={this.state.random} />
+        <ScrollView
+          style={{width: '100%'}}
+          //? 스크롤하고 난 후 손을 떼었을때 trigger
+          // onMomentumScrollBegin={() => alert('begin')}
+          //? 스크롤 바운스가 멈췄을때 trigger
+          // onMomentumScrollEnd={() => alert('end')}
+          //? 스크롤 움직이자마자 trigger
+          // onScroll={() => alert('scrolling')}
+          //? 스크롤뷰가 사이즈가 변경되었을때 trigger 인자로는 스크롤뷰의 width와 height값을 전달해준다.
+          // onContentSizeChange={(width, height) => {
+          //   alert(height);
+          // }}
+          //? ios에서 스크롤 끝에서 스크롤을 실행할때 bounce 효과설정
+          bounces={false}>
+          <Generator onAddRandomNum={this.onAddRandomNum} />
+          <NumList onNumDelete={this.onNumDelete} numbers={this.state.random} />
+        </ScrollView>
       </View>
     );
   }
